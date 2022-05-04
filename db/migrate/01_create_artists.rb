a7706253989b36ef105fde2701e4f0bafe70ce0c
seth <seth623@gmail.com>
2022-05-03 19:53:08 -0600

    Completed Assignment

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,36 @@
+#ActiveRecord::Base.establish_connection(
+#    :adapter => "sqlite3",
+#    :database => "db/artists.sqlite"
+#)
+
+#sql = <<-SQL
+#    CREATE TABLE IF NOT EXISTS artists(
+#        id INTEGER PRIMARY KEY,
+#        name TEXT, 
+#        genre TEXT,
+#        age INTEGER,
+#        hometown TEXT
+#    );
+#SQL
+
+#ActiveRecord::Base.connection.execute(sql)
+
+
+class CreateArtists < ActiveRecord::Migration[4.2]
+
+    def change
+        create_table :artists do |t|
+            t.string :name
+            t.string :genre 
+            t.integer :age 
+            t.string :hometown 
+        end 
+    end 
+
+    def up
+    end
+
+    def down
+    end 
+
+end 
